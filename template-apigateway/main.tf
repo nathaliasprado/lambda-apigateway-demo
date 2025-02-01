2dbc88f6ac1f94a86fc57f60e0f238167722860f
nathaliasprado <nathaliaprado94@icloud.com>
2025-02-01 10:51:37 -0300

    feat: create variables.tf

--- template-apigateway/main.tf
+++ template-apigateway/main.tf
@@ -1,43 +1,42 @@
 resource "aws_api_gateway_rest_api" "apigateway" {
-  name = "DynamoDBOperations"
+  name = var.name
 
   endpoint_configuration {
-    types = ["REGIONAL"]
+    types = var.types
   }
 }
 
 resource "aws_api_gateway_resource" "resource" {
   rest_api_id = aws_api_gateway_rest_api.apigateway.id
   parent_id   = aws_api_gateway_rest_api.apigateway.root_resource_id
-  path_part   = "DynamoDBManager"
+  path_part   = var.path_part
 }
 
 resource "aws_api_gateway_method" "method" {
   rest_api_id   = aws_api_gateway_rest_api.apigateway.id
   resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = "POST"
-  authorization = "NONE"
+  http_method   = var.http_method
+  authorization = var.authorization
 }
 
-resource "aws_lambda_permission" "apigw_lambda" {
+resource "aws_lambda_permission" "lambda_permission" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = "LambdaFunctionOverHttps"
+  function_name = var.function_name
   principal     = "apigateway.amazonaws.com"
 
-  #   source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.apigateway.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
-  source_arn = "arn:aws:execute-api:us-east-1:660193330720:${aws_api_gateway_rest_api.apigateway.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+  source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.apigateway.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
 }
 
 resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = aws_api_gateway_rest_api.apigateway.id
   resource_id             = aws_api_gateway_resource.resource.id
   http_method             = aws_api_gateway_method.method.http_method
-  integration_http_method = "POST"
+  integration_http_method = var.integration_http_method
   type                    = "AWS_PROXY"
-  uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:660193330720:function:LambdaFunctionOverHttps/invocations" // OMITIR ESSE VALOR ANTES DE TORNAR O REPO PÚBLICO
+  uri                     = "arn:aws:apigateway:${var.myregion}:lambda:path/2015-03-31/functions/arn:aws:lambda:${var.myregion}:${var.accountId}:function:${var.function_name}/invocations"
 
-  depends_on = [aws_lambda_permission.apigw_lambda]
+  depends_on = [aws_lambda_permission.lambda_permission]
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
@@ -60,5 +59,5 @@ resource "aws_api_gateway_deployment" "deployment" {
 resource "aws_api_gateway_stage" "stage" {
   deployment_id = aws_api_gateway_deployment.deployment.id
   rest_api_id   = aws_api_gateway_rest_api.apigateway.id
-  stage_name    = "dev"
+  stage_name    = var.stage_name
 }
---
+++ template-apigateway/variables.tf
@@ -0,0 +1,59 @@
+variable "name" {
+  description = "Name of the REST API."
+  type        = string
+  default     = "DynamoDBOperations"
+}
+
+variable "types" {
+  description = "List of endpoint types. Valid values: EDGE, REGIONAL or PRIVATE. "
+  type        = list(string)
+  default     = ["REGIONAL"]
+}
+
+variable "path_part" {
+  description = "Last path segment of this API resource."
+  type        = string
+  default     = "DynamoDBManager"
+}
+
+variable "http_method" {
+  description = "HTTP Method (GET, POST, PUT, DELETE, HEAD, OPTIONS, ANY)."
+  type        = string
+  default     = "POST"
+}
+
+variable "authorization" {
+  description = "Type of authorization used for the method (NONE, CUSTOM, AWS_IAM, COGNITO_USER_POOLS)."
+  type        = string
+  default     = "NONE"
+}
+
+variable "function_name" {
+  description = "Name of the Lambda Function."
+  type        = string
+  default     = "LambdaFunctionOverHttps"
+}
+
+variable "myregion" {
+  description = "Region used to create the resources."
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "accountId" {
+  description = "Account ID."
+  type        = number
+  default     = 000000000000
+}
+
+variable "integration_http_method" {
+  description = "Integration HTTP method (GET, POST, PUT, DELETE, HEAD, OPTIONs, ANY, PATCH) specifying how API Gateway will interact with the back end."
+  type        = string
+  default     = "POST"
+}
+
+variable "stage_name" {
+  description = "Name of the stage."
+  type        = string
+  default     = "dev"
+}
